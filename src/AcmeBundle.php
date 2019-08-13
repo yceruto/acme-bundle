ee7621ff83a0fccf718da78d15b2ecb73b149ef8
Yonel Ceruto <yonelceruto@gmail.com>
2019-08-13 14:39:47 -0400

    mixed version (all versions compat)

--- src/AcmeBundle.php
+++ src/AcmeBundle.php
@@ -3,11 +3,12 @@
 namespace Acme\AcmeBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AcmeBundle extends Bundle
 {
     public function getPath()
     {
-        return \dirname(__DIR__);
+        return Kernel::VERSION_ID >= 40400 ? \dirname(__DIR__) : __DIR__;
     }
 }
